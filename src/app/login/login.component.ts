2c30980e06c3b5dd05a6cd1e6122f5154485f60e
barthaleventegabor <b.levi.levi@gmail.com>
2026-02-03 11:00:37 +0100

    Bejelentkezés

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private readonly authService = inject(AuthService);
+  private readonly auth = inject(AuthService);
   private readonly builder = inject(FormBuilder);
   private readonly router = inject(Router);
 
@@ -20,6 +20,24 @@ export class LoginComponent {
   });
 
   login() {
+    console.log("Belépsés...");
+    console.log(this.loginForm.value);
+    this.auth.login(this.loginForm.value).subscribe({
+      next: (result:any) => {
+        if(result.success){
+          const token = result.data[0].token;
+          console.log(token);
+          localStorage.setItem('token', token);
+          this.auth.loginSuccess();
+          this.router.navigate(['/drink']);
+        }else{
+          console.log("Nemnem")
+        }
 
+      },
+      error: (err) => {
+        
+      }
+    });
   }
 }
